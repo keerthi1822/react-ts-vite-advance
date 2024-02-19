4c88d572f55148d3b61f68cad6a7b85f7f8ffa81
keerthi1822 <keerthi1822@gmail.com>
2024-02-19 20:32:12 +0100

    first commit with some code

---
+++ src/components/NewNote.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NoteForm  from './NoteForm'
+
+const NewNote = () => {
+  return (
+    <div>
+        <div className='mb-4'>NewNote</div>
+        <NoteForm />
+    </div>
+    
+
+  )
+}
+
+export default NewNote
